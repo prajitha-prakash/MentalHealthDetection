a5a18a8fe945e7205b4a32dc132439138c25ab5c
Navya Narayanan <navyanarayanan624@gmail.com>
2023-03-06 11:15:32 +0530

    file added'

--- emotion identification/login.py
+++ emotion identification/login.py
@@ -78,6 +78,41 @@ def emotion_finding():
 
                 # prevents openCL usage and unnecessary logging messages
                 cv2.ocl.setUseOpenCL(False)
+                # dictionary which assigns each label an emotion (alphabetical order)
+                emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral",
+                                5: "Sad",
+                                6: "Surprised"}
+
+                frame = cv2.imread(attendence_pic_path + filename)
+
+                facecasc = cv2.CascadeClassifier(
+                    r'C:\mental\haarcascade_frontalface_default.xml')
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                faces = facecasc.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+
+                for (x, y, w, h) in faces:
+                    cv2.rectangle(frame, (x, y - 50), (x + w, y + h + 10), (255, 0, 0), 2)
+                    roi_gray = gray[y:y + h, x:x + w]
+                    cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1),
+                                                 0)
+                    prediction = model.predict(cropped_img)
+                    # print(prediction)
+                    maxindex = int(np.argmax(prediction))
+                    print(emotion_dict[maxindex])
+                    res_emo = emotion_dict[maxindex]
+                    print("Detected Emotion  :  ", res_emo)
+            return "ok"
+            except(KeyboardInterrupt):
+            print("Turning off camera.")
+            webcam.release()
+            print("Camera off.")
+            print("Program ended.")
+            cv2.destroyAllWindows()
+            # break
+
+
+while True:
+    emotion_finding()
 
 
 
